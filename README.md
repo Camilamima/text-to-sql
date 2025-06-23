34aef61d4704f12dd034007cd79b0ebb4a7e908a
Camilamima <cml.marani@gmail.com>
2025-06-23 20:17:45 -0300

    Versão final, adicionado tratamento de erros, mudado DER

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Projeto text-to-sql
 
 ## 📋 Sobre o Projeto
-Este projeto cria uma conversão de linguagem natural para SQL utilizando LLM.
+Este projeto cria uma conversão de linguagem natural para SQL utilizando LLM. Projeto feito para a matéria de Banco de Dados da UTFPR.
 
 ## 🔧 Tecnologias Utilizadas
 - Python 3.8+
@@ -18,10 +18,21 @@ Este projeto cria uma conversão de linguagem natural para SQL utilizando LLM.
 ## 🚀 Como Executar
 
 ### Pré-requisitos
+- Crie um servidor MySQL e Potsgre dentro do seu computador
 - Instale as blibliotecas necessárias com o comando: pip install "bliblioteca aqui".
-- No site da groq, crie uma conta e baixe uma API:
-- Substitua a api_key pela sua api
-- Instale os bancos de dados (PostgreSQL e MySQL) e substitua as informações aqui:
+- No site da groq, crie uma conta e crie sua api aqui:
+  <img width="820" alt="image" src="https://github.com/user-attachments/assets/2df5a19b-bed5-4f00-bf18-2fef04d21462" />
+
+- Crie o banco de dados usando os comandos SQL(tanto no MySQL quanto no Postgre) presentes aqui: (https://db-book.com/university-lab-dir/sample_tables-dir/index.html)
+- Caso utilize outro banco de dados que não seja o University, coloque um link com o DER novo aqui(ignore essa parte caso seja o mesmo banco de dados do passo anterior):
+
+  <img width="509" alt="image" src="https://github.com/user-attachments/assets/68f3d6be-51cf-427a-8d87-42d6703be4c4" />
+
+
+- Substitua o arquivo key abaixo com o diretorio do seu txt contendo sua api_key:
+  <img width="548" alt="image" src="https://github.com/user-attachments/assets/053adb14-d1ab-4486-95e2-fbc3637ba6ae" />
+
+- Substitua as informações do seu banco de dados nas seguintes linhas de código:
   ```bash
   if(bd==1):
     import MySQLdb as my
@@ -45,6 +56,6 @@ Este projeto cria uma conversão de linguagem natural para SQL utilizando LLM.
     )
     banco_de_dados=" o banco é PostgreSQL"
 '''
-- Crie o banco de dados usando os comandos SQL presentes aqui: (https://db-book.com/university-lab-dir/sample_tables-dir/index.html)
+- Agora é só rodar e brincar
 ### 📖 Schema do banco de dado utilizado (university):
-![Diagrama do Banco de Dados](https://raw.githubusercontent.com/Camilamima/text-to-sql/refs/heads/main/banco_de_dados.jpg)
+![Diagrama do Banco de Dados](https://raw.githubusercontent.com/Camilamima/text-to-sql/refs/heads/main/banco_de_dados.png)
--- text-to-sql.py
+++ text-to-sql.py
@@ -8,11 +8,15 @@ import psycopg2 as psy
 warnings.filterwarnings("ignore", message="pandas only supports SQLAlchemy connectable")
 
 ###URL do schema###
-schema="https://github.com/Camilamima/text-to-sql/blob/main/banco_de_dados.jpg?raw=true"
+schema="https://github.com/Camilamima/text-to-sql/blob/main/banco_de_dados.png?raw=true"
+
+####Le a chave API pra não cair mais no github####
+with open(r"C:\Users\admin\Desktop\banco de dados\api_key.txt", "r") as arquivo:
+    key = arquivo.read().strip()  # Lê a chave da API do arquivo
 
 ####conexão ao Groq####
 client = Groq(
-    api_key="-----",
+    api_key=key,
 )
 
 ####Escolha de banco de dados####
@@ -31,6 +35,7 @@ if(bd==1):
     )
     banco_de_dados=" o banco é MySql"
     cursor=conexao.cursor()
+
 
 elif(bd==2):
     import psycopg2 as psy
@@ -55,19 +60,15 @@ while(opcao!=4):
     match opcao:
         case 1: #Printra todas as tabelas atrávez de um looping
 
-            tabelas = [
-            "prereq",
-            "time_slot",
-            "advisor",
-            "takes",
-            "student",
-            "teaches",
-            "section",
-            "instructor",
-            "course",
-            "department",
-            "classroom"
-            ]
+            ###Dependendo do banco de dados, faz uma lista com as tabelas disponíveis###
+            if bd==1:
+                query_tabelas="SELECT table_name FROM information_schema.tables WHERE table_schema = 'university';"
+            else:
+                query_tabelas="SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';"
+            
+            cursor.execute(query_tabelas)
+            tabelas = [t[0] for t in query_tabelas]
+                
             for i in tabelas:
                 query=f"SELECT * FROM {i} LIMIT 3"
                 df=pd.read_sql(query,conexao)
@@ -82,7 +83,7 @@ while(opcao!=4):
             chat_resposta=client.chat.completions.create(
                messages=[
                     {"role": "system",
-                    "content": "Você é um assistente de banco de dados, você irá receber perguntas em linguagem natural e deve responder com apenas a query SQL, sem nada a mais, caso a pergunta não seja relacionada ao banco de dados University, responda com 1." + banco_de_dados
+                    "content": "Você é um assistente de banco de dados, você irá receber perguntas em linguagem natural e deve responder com apenas a query SQL, sem nada a mais, caso a pergunta não seja relacionada ao banco de dados mandado, responda com 1." + banco_de_dados
                    },##Aqui foi dito ao sistema qual é o seu papel##
                    {"role": "user",
                         "content": [
@@ -103,6 +104,7 @@ while(opcao!=4):
             
             ##Printa a query##
             query=chat_resposta.choices[0].message.content
+            query = query.replace("```sql", "").replace("```", "").strip()
             print("\n",query)
 
             if(query.strip() =="1"):
@@ -126,7 +128,7 @@ while(opcao!=4):
                     conexao.commit()
                     print("Query executada com sucesso")
                 except Exception as error:
-                    print("Ocorreu um erro, query errada")
+                    print("Ocorreu um erro ao executar a query:", error)
             
            
 
